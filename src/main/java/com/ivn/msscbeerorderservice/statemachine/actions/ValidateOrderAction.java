283a8e26b66d1231ac2bedb09b26abcabb54008d
Ivan Ivanyshyn <ivan.ivanyshyn@cgm.com>
2022-01-17 13:21:33 +0200

    adding Validation Order Request action, closes #15

---
+++ src/main/java/com/ivn/msscbeerorderservice/statemachine/actions/ValidateOrderAction.java
@@ -0,0 +1,40 @@
+package com.ivn.msscbeerorderservice.statemachine.actions;
+
+import com.ivn.brewery.model.events.ValidateOrderRequest;
+import com.ivn.msscbeerorderservice.config.JmsConfig;
+import com.ivn.msscbeerorderservice.domain.BeerOrder;
+import com.ivn.msscbeerorderservice.domain.BeerOrderEventEnum;
+import com.ivn.msscbeerorderservice.domain.BeerOrderStatusEnum;
+import com.ivn.msscbeerorderservice.repositories.BeerOrderRepository;
+import com.ivn.msscbeerorderservice.services.BeerOrderManagerImpl;
+import com.ivn.msscbeerorderservice.web.mappers.BeerOrderMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.statemachine.StateContext;
+import org.springframework.statemachine.action.Action;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ValidateOrderAction implements Action<BeerOrderStatusEnum, BeerOrderEventEnum> {
+
+    private final BeerOrderRepository beerOrderRepository;
+    private final BeerOrderMapper beerOrderMapper;
+    private final JmsTemplate jmsTemplate;
+
+    @Override
+    public void execute(StateContext<BeerOrderStatusEnum, BeerOrderEventEnum> context) {
+        String beerOrderId = (String) context.getMessage().getHeaders().get(BeerOrderManagerImpl.ORDER_ID_HEADER);
+        BeerOrder beerOrder = beerOrderRepository.findOneById(UUID.fromString(beerOrderId));
+
+        jmsTemplate.convertAndSend(JmsConfig.VALIDATE_ORDER_QUEUE, ValidateOrderRequest.builder()
+                .beerOrder(beerOrderMapper.beerOrderToDto(beerOrder))
+                .build());
+        log.debug("Sent Validation request to queue for order id " + beerOrderId);
+
+    }
+}
