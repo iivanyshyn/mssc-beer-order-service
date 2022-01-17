92d681451bde1ede1eba5c0e75a9c0653ccbd2fb
Ivan Ivanyshyn <ivan.ivanyshyn@cgm.com>
2022-01-17 14:36:07 +0200

    adding validate order listener, closes #17

---
+++ src/main/java/com/ivn/brewery/model/events/ValidateOrderResult.java
@@ -0,0 +1,18 @@
+package com.ivn.brewery.model.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ValidateOrderResult {
+
+    private UUID orderId;
+    private Boolean isValid;
+}
